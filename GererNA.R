0fdfff42f087db8e4f879a6c85fb5838289fd8e8
feronromane <148044017+feronromane@users.noreply.github.com>
2024-01-02 22:32:48 +0100

    Update GererNA.R

--- GererNA.R
+++ GererNA.R
@@ -27,18 +27,18 @@ mur_sud <- mur_sud %>%
 
 
 #MUR 1
-#Imputation par la moyenne pour les NA au début (6% + 2%)
+# Interpolation pour les valeurs manquantes au milieu de la série temporelle
+mur_1$Mur_Temp_1_1.mean <- na.approx(mur_1$Mur_Temp_1_1.mean, na.rm = FALSE)
+mur_1$Mur_Temp_1_2.mean <- na.approx(mur_1$Mur_Temp_1_2.mean, na.rm = FALSE)
+mur_1$Mur_Temp_1_3.mean <- na.approx(mur_1$Mur_Temp_1_3.mean, na.rm = FALSE)
+#Imputation par la moyenne pour les NA (6% + 2%)
 mur_1$Mur_Temp_1_1.mean[1:33089] <- mean(mur_1$Mur_Temp_1_1.mean, na.rm = TRUE)
 mur_1$Mur_Temp_1_2.mean[1:33089] <- mean(mur_1$Mur_Temp_1_2.mean, na.rm = TRUE)
 mur_1$Mur_Temp_1_3.mean[1:33089] <- mean(mur_1$Mur_Temp_1_3.mean, na.rm = TRUE)
-#Imputation par regression
-model <- lm(Mur_Temp_1_1.mean ~ Mur_Temp_1_2.mean, data = mur_1, subset = !is.na(Mur_Temp_1_1.mean))
-indices_a_imputer <- 48004:59322
-mur_1$Mur_Temp_1_1.mean[indices_a_imputer] <- predict(model, newdata = mur_1[indices_a_imputer, ])
-model_2 <- lm(Mur_Temp_1_2.mean ~ Mur_Temp_1_3.mean, data = mur_1, subset = !is.na(Mur_Temp_1_2.mean))
-mur_1$Mur_Temp_1_2.mean[indices_a_imputer] <- predict(model_2, newdata = mur_1[indices_a_imputer, ])
-model_3 <- lm(Mur_Temp_1_2.mean ~ Mur_Temp_1_3.mean, data = mur_1, subset = !is.na(Mur_Temp_1_2.mean))
-mur_1$Mur_Temp_1_2.mean[indices_a_imputer] <- predict(model_3, newdata = mur_1[indices_a_imputer, ])
+mur_1$Mur_Temp_1_1.mean[48004:59322] <- mean(mur_1$Mur_Temp_1_1.mean, na.rm = TRUE)
+mur_1$Mur_Temp_1_2.mean[48004:59322] <- mean(mur_1$Mur_Temp_1_2.mean, na.rm = TRUE)
+mur_1$Mur_Temp_1_3.mean[48004:59322] <- mean(mur_1$Mur_Temp_1_3.mean, na.rm = TRUE)
+#mur_1 ne contient plus de NA.
 
 #MUR 2
 # Interpolation pour les valeurs manquantes au milieu de la série temporelle
