9145efddb8b24ca79df9b56535c1e85a0fd837cb
louiscampiglia <148078684+louiscampiglia@users.noreply.github.com>
2024-01-03 12:26:27 +0100

    Les valeurs absurdes de températures intérieure (-20°C à -250°C) ajoutées aux NA (avant le traitement des NA)

--- GererNA.R
+++ GererNA.R
@@ -27,6 +27,9 @@ mur_sud <- mur_sud %>%
 
 
 #MUR 1
+#On a un faible nombre de valeurs de température intérieure absurdes (de -20°C à -250°C), on les rajoute aux NA
+mur_1$Mur_Temp_1_1.mean[mur_1$Mur_Temp_1_1.mean<0]<-NA
+mur_1$Mur_Temp_1_2.mean[mur_1$Mur_Temp_1_2.mean<0]<-NA
 # Interpolation pour les valeurs manquantes au milieu de la série temporelle
 mur_1$Mur_Temp_1_1.mean <- na.approx(mur_1$Mur_Temp_1_1.mean, na.rm = FALSE)
 mur_1$Mur_Temp_1_2.mean <- na.approx(mur_1$Mur_Temp_1_2.mean, na.rm = FALSE)
@@ -39,8 +42,6 @@ mur_1$Mur_Temp_1_1.mean[48004:59322] <- mean(mur_1$Mur_Temp_1_1.mean, na.rm = TR
 mur_1$Mur_Temp_1_2.mean[48004:59322] <- mean(mur_1$Mur_Temp_1_2.mean, na.rm = TRUE)
 mur_1$Mur_Temp_1_3.mean[48004:59322] <- mean(mur_1$Mur_Temp_1_3.mean, na.rm = TRUE)
 #mur_1 ne contient plus de NA.
-#On a 2 valeurs de température intérieure absurdes (-20°C et -60°C), on les remplaces par la moyenne
-mur_1$Mur_Temp_1_1.mean[mur_1$Mur_Temp_1_1.mean<0]<-mean(mur_1$Mur_Temp_1_1.mean, na.rm = TRUE)
 
 #MUR 2
 # Interpolation pour les valeurs manquantes au milieu de la série temporelle
